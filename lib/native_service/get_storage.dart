b4cee9f7b2fdf3074840377e44caabf810c0e872
oulanassri <oulanassri@gmail.com>
2025-07-03 19:02:59 +0200

    Update get_storage.dart

--- lib/native_service/get_storage.dart
+++ lib/native_service/get_storage.dart
@@ -3,14 +3,15 @@ import 'package:get_storage/get_storage.dart';
 class UserStorage {
   static GetStorage storage = GetStorage();
 
- static void save(String key, String value)  {
-     storage.write(key, value);
+  static void save(String key, String value) {
+    storage.write(key, value);
   }
 
-  static String read(String key)  {
-    return  storage.read(key);
+  static String read(String key) {
+    return storage.read(key) ?? ''; // ✅ آمن ضد null
   }
-  static void delete(String key)  {
-      storage.write(key,'');
+
+  static void delete(String key) {
+    storage.remove(key); // ✅ إزالة المفتاح بالكامل
   }
 }
